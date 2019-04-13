a4ef2d5aa0b2e379fda8e0c61db949c2531f9d48
Danis Pangestu <48709739+DanisPangestu@users.noreply.github.com>
2019-04-13 17:26:55 +0700

    Update MainActivity.java

--- app/src/main/java/pangestudanis/gmail/com/MainActivity.java
+++ app/src/main/java/pangestudanis/gmail/com/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //script untuk memberi edittext dan butoon  serta memberi perintah on click pada button supaya bisa melakukan intent
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
         btnLogin = (Button)findViewById(R.id.btnLogin);
@@ -24,19 +24,19 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //script tipe data string untuk memberi inputan huruf serta angka di kollom edit text dan mendapatkan nilai dikolom tersebut
                 String usernameKey = username.getText().toString();
                 String passwordKey = password.getText().toString();
 
                 if (usernameKey.equals("danis") && passwordKey.equals("165410156")){
-                    //jika login berhasil
+                    //script login sukses jika login yang kita iput kan sama dengan di script
                     Toast.makeText(getApplicationContext(), "LOGIN SUKSES",
                             Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(MainActivity.this, View_Nama_Danis.class);
                     MainActivity.this.startActivity(intent);
                     finish();
                 }else {
-                    //jika login gagal
+                    //script jika login yang kita inputkan salah, maka akan muncul dialog username anda salah
                     AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                     builder.setMessage("Username atau Password Anda salah!")
                             .setNegativeButton("Retry", null).create().show();
