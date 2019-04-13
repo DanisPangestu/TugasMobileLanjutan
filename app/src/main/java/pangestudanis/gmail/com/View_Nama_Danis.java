e1fc53d0febfb9a34105335c9aef351bfee84e63
Danis Pangestu <48709739+DanisPangestu@users.noreply.github.com>
2019-04-13 17:35:25 +0700

    Update View_Nama_Danis.java

--- app/src/main/java/pangestudanis/gmail/com/View_Nama_Danis.java
+++ app/src/main/java/pangestudanis/gmail/com/View_Nama_Danis.java
@@ -1,14 +1,15 @@
-package pangestudanis.gmail.com;
-
+package pangestudanis.gmail.com; //nama package saya
+//import yang dibutuhkan di java ini
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
-
+//script menjelaskan tentang class yang bernama View_nama_dani, serta mengimport fungsi listview 
 public class View_Nama_Danis extends AppCompatActivity {
     ListView list;
+    //script dibawah ini merupakan isi dari list view,maintitle,subtitle,serta gambar dengan tipe data string dan integer
     String[] maintitle = {
             "Danis Pangestu // 165410156", "Bisri Hanafi ",
             "Mikael Dionito Harbigantara", "Rahmat Mustaqim",
@@ -27,7 +28,7 @@ public class View_Nama_Danis extends AppCompatActivity {
             R.drawable.coba1,R.drawable.coba2,
             R.drawable.coba4, R.drawable.coba3,
     };
-
+    //script dibawah ini melakukan perintah untuk memperlihatkan bagian atas di layar aplikasi kita.serta melakukan toast jika bagian text/gambar disentuh
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,10 +41,9 @@ public class View_Nama_Danis extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 // TODO Auto-generated method stub
                 if (position == 0) {
-//code specific to first list item
+//script untuk melakukan toast dan memberi kalimat ke si pengguna jika ia menekan nya
                     Toast.makeText(getApplicationContext(), "terima kasih telah memilih saya", Toast.LENGTH_SHORT).show();
                 } else if (position == 1) {
-//code specific to 2nd list item
                     Toast.makeText(getApplicationContext(), "terima kasih telah memilih saya", Toast.LENGTH_SHORT).show();
                 } else if (position == 2) {
                     Toast.makeText(getApplicationContext(), "terima kasih telah memilih saya", Toast.LENGTH_SHORT).show();
